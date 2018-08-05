3608fecf87410f439a654b446c1bcf27e9594905
han0ah <han0ah92@gmail.com>
2018-08-06 00:43:25 +0900

    Remove comment in setup_all.sh to download Glove

--- setup_all.sh
+++ setup_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Download pretrained embeddings.
-#curl -O http://lsz-gpu-01.cs.washington.edu/resources/glove_50_300_2.txt
-#curl -O https://nlp.stanford.edu/data/glove.840B.300d.zip
-#unzip glove.840B.300d.zip
-#rm glove.840B.300d.zip
+curl -O http://lsz-gpu-01.cs.washington.edu/resources/glove_50_300_2.txt
+curl -O https://nlp.stanford.edu/data/glove.840B.300d.zip
+unzip glove.840B.300d.zip
+rm glove.840B.300d.zip
 
 # Build custom kernels.
 TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
